1293b7fc7c27d828d92a872c7d46c515c88feca8
Andrii Zhupyk <madzhup@gmail.com>
2021-02-23 13:21:50 +0200

    chore: update dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import SVGTranslator from 'svg-scaler-viewbox';
 import { getOptions } from 'loader-utils';
-import validateOptions from 'schema-utils';
+import { validate } from 'schema-utils';
 
 const schema = {
   type: 'object',
@@ -20,7 +20,7 @@ const schema = {
 export default function SvgScalerLoader(source) {
   const options = getOptions(this);
 
-  validateOptions(schema, options, 'Webpack SVG scaler Loader');
+  validate(schema, options, 'Webpack SVG scaler Loader');
 
   const callback = this.async();
   const svgTranslator = new SVGTranslator(options);
